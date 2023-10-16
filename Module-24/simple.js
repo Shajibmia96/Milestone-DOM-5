85fdcee455587dc8fc708d7a1d757aef895eb8e1
Shajib96 <shajibahamedornob@gmail.com>
2023-10-16 07:50:47 +0600

    module-24-1 done

---
+++ Module-24/simple.js
@@ -0,0 +1,13 @@
+console.log("Hello , Iam Shajib Sarkar");
+console.dir('Bangladesh is a small country');
+const liCollection = document.getElementsByTagName('li')
+// console.log(liCollection);
+for( const li of liCollection){
+    // console.log(li.innerText)
+}
+const allh1 = document.getElementsByTagName('h1');
+for(h1 of allh1){
+    // console.log(h1.innerText)
+}
+
+
