4c55e8de83c005cfb83ffded0306fb6e88a463e7
Shajib96 <shajibahamedornob@gmail.com>
2023-10-19 11:43:30 +0600

    add module.25-6

--- Module-25/js/event.js
+++ Module-25/js/event.js
@@ -1,3 +1,7 @@
+function makeMeRed(){
+    document.body.style.backgroundColor ="red";
+}
+
 const makeBlue = document.getElementById("makeMeBlue");
 makeBlue.onclick = makesMeBlue;
 function makesMeBlue(){
@@ -11,3 +15,7 @@ const makePurple = document.getElementById ("makeMePurple").onclick = function()
 document.getElementById ("count").onclick = function(){
     console.log("X")
 }
+
+document.getElementById("make-Green").addEventListener('click',function(){
+    document.body.style.backgroundColor = "Green";
+})
