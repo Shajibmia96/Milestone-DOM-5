160d4f6024f4544217c964b160ad859b67dd2952
Shajib96 <shajibahamedornob@gmail.com>
2023-10-27 11:36:25 +0600

    added conceptual sesstion

---
+++ Module-25/js/callback.js
@@ -0,0 +1,8 @@
+function uperName (name){
+   console.log(uperName.toUpperCase)
+}
+function fullName(first , uperName){
+    const fullName = first + 'Sarkar';
+    uperName(fullName);
+}
+fullName (Shajib , uperName);
---
+++ Module-25/js/project/index.js
@@ -0,0 +1,49 @@
+// count = 0
+// const btn = document.getElementById("btn-plus").addEventListener("click", function(){
+    
+//     var element = document.getElementById('count');
+//     count +=1;
+//     element.innerText = count;
+    
+// })
+// const btnOne = document.getElementById("btn-minus").addEventListener("click", function(){
+    
+//     var element = document.getElementById('count');
+//     count -=1;
+//     element.innerText = count;
+    
+// })
+
+
+//project done
+
+
+// document.getElementById('btn-submit').addEventListener('click',function(){
+// //    console.log("hello")
+// const textAria = document.getElementById('text')
+// const textValue = textAria.value;
+// // console.log(textValue);
+// const container =document.getElementById("user-review")
+// const p =document.createElement('p')
+// p.innerText = textValue;
+// container.appendChild(p);
+//  textAria.value = '';
+// })
+
+
+// document.getElementById('text').addEventListener('keyup' , function(event){
+// //    console.log(event.target.value)
+  
+// if(event.key ==='Enter'){
+//   const textAria = document.getElementById('text');
+//   const textValue = textAria.value;
+// //   console.log(textValue)
+
+//  const pg = document.createElement('p')
+//  pg.innerText = textValue;
+//  const container =document.getElementById("user-review")
+//  container.appendChild(pg);
+//  textAria.value = '';
+// }
+// })
+
